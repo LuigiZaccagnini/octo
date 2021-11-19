5708b87588bb27332099a24bdd11df8568d0b0ac
Luigi Zaccagnini <l_zaccagnini_98@hotmail.com>
2021-11-18 22:35:53 -0500

    Added tests for lineChecker

--- test/fileFunction.test.js
+++ test/fileFunction.test.js
@@ -8,19 +8,26 @@ test('Checks if it can add a directory', () => {
 
 it('Markdown to HTML renders correctly', () => { 
 ff.markdownToHTML('test/markdownTest.md').then(html => {
-  const tree = ReactTestRenderer
-    .create(html)
-    .toJSON();
+  const tree = ReactTestRenderer.create(html).toJSON();
   expect(tree).toMatchSnapshot();
   });
 });
 
 it('Markdown to HTML renders correctly with language option', () => {
 ff.markdownToHTML('test/markdownTest.md', 'It').then(html => {
-  const tree = ReactTestRenderer
-    .create(html)
-    .toJSON();
+  const tree = ReactTestRenderer.create(html).toJSON();
   expect(tree).toMatchSnapshot();
   });
 });
 
+test('Checks if it lineChecker filter without first line', () => {
+  expect(ff.lineChecker('Hello World', false)).toBe('<p>Hello World</p>');
+});
+
+test('Checks if it lineChecker filter with first line', () => {
+  expect(ff.lineChecker('Hello World', true)).toBe('<h1>Hello World</h1>');
+});
+
+test('Checks if it lineChecker filters a empty line', () => {
+  expect(ff.lineChecker('', false)).toBe('<br />');
+});
